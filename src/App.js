caba8dbd93458420c02b2e2099a53e17a8a60462
cbass2404 <cory.bass.07@gmail.com>
2021-09-13 09:57:28 -0500

    initial section complete

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { useState } from 'react';
+import './App.css';
 
 function App() {
-  return <div className="App"></div>;
+  const [color, setColor] = useState('red');
+  const [disabled, setDisabled] = useState(false);
+
+  const handleClick = () => {
+    const newColor = color === 'red' ? 'blue' : 'red';
+    setColor(newColor);
+  };
+
+  return (
+    <div className='App'>
+      <button
+        style={{ backgroundColor: color }}
+        onClick={handleClick}
+        disabled={disabled}
+      >
+        {color === 'red' ? 'Change to blue' : 'Change to red'}
+      </button>
+      <input
+        id='enable-button-checkbox'
+        type='checkbox'
+        checked={disabled}
+        onClick={() => setDisabled(!disabled)}
+      />
+    </div>
+  );
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,53 @@
-import { render, screen } from "@testing-library/react";
-import App from "./App";
+import { render, screen, fireEvent } from '@testing-library/react';
+import App from './App';
 
-test("renders learn react link", () => {
+let colorButton;
+let checkBox;
+
+beforeEach(() => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  // find an element with a role of button and text of 'Change to blue'
+  colorButton = screen.getByRole('button', { name: /Change to blue/i });
+  checkBox = screen.getByRole('checkbox');
+});
+
+describe(`App component has button and functions as follows`, () => {
+  it('has the correct initial color', () => {
+    // expect the background color to be red
+    expect(colorButton).toHaveStyle({ backgroundColor: 'red' });
+  });
+
+  it('turns blue when clicked', () => {
+    // click button
+    fireEvent.click(colorButton);
+
+    // expect the background color to be blue
+    expect(colorButton).toHaveStyle({ backgroundColor: 'blue' });
+  });
+
+  it('has the correct initial text', () => {
+    fireEvent.click(colorButton);
+
+    expect(colorButton.textContent).toBe('Change to red');
+  });
+
+  it(`starts out enabled`, () => {
+    expect(colorButton).toBeEnabled();
+  });
+
+  it(`has a checkbox that starts unchecked`, () => {
+    expect(checkBox).not.toBeChecked();
+  });
+
+  it(`is disabled if checkbox is clicked`, () => {
+    fireEvent.click(checkBox);
+
+    expect(colorButton).toBeDisabled();
+  });
+
+  it('has a checkbox that is checked when clicked', () => {
+    fireEvent.click(checkBox);
+
+    expect(checkBox).toBeChecked();
+  });
 });
